0bbb7e38c3a3fd43483291e7c4b36c67a2a456a9
Victor <victorcoc@icloud.com>
2025-03-17 20:41:24 +0100

    Compilation and test that everything works on the target after misraC compliance modification

--- RTE/Device/STM32L475VGTx/STCubeGenerated/Core/Src/main.c
+++ RTE/Device/STM32L475VGTx/STCubeGenerated/Core/Src/main.c
@@ -654,7 +654,7 @@ static void MX_GPIO_Init(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-static void Error_Handler(void)
+void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
